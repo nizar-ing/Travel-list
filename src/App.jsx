6e95d5964ac09f67ee109f842ea68113ddbfc467
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-06-24 00:20:38 +0100

    Implementing the Stats component by using derived stats

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
             <Logo/>
             <Form onAddItem={handleAddItem} />
             <PackingList items={items} onDeleteItem={handleDeleteItem} onCheckedItem={handleCheckedItem} />
-            <Stats />
+            <Stats items={items} />
         </div>
     );
 };
@@ -96,11 +96,24 @@ const Item = ({item, onDeleteItem, onCheckedItem}) => {
     );
 }
 
-const Stats = () => {
+const Stats = ({items}) => {
+
+    if(!items.length){
+        return (
+            <footer className="stats">
+                <em>
+                    Start adding some items in your packing list 🚀
+                </em>
+            </footer>
+        );
+    }
+    const countItems = items.length;
+    const packedItems = items.filter((item) => item.packed).length;
+    const percentage = Math.round((packedItems/countItems) * 100);
     return (
         <footer className="stats">
             <em>
-                💼 You have X items on your list, you already packed X (X%).
+                { percentage === 100 ? 'You got everything! Ready to go. ✈' : `💼 You have ${countItems} items on your list, you already packed ${packedItems} (${percentage}%).`}
             </em>
         </footer>
     );
