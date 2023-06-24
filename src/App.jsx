3a884b361cce3490feb3b8be421191a124911951
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-06-24 22:02:14 +0100

    Implementing the sort feature

--- src/App.jsx
+++ src/App.jsx
@@ -72,13 +72,27 @@ const Form = ({onAddItem}) => {
 }
 
 const PackingList = ({items, onDeleteItem, onCheckedItem}) => {
+    const[sortBy, setSortBy] = useState("input");
+
+    let sortedItems;
+    if(sortBy === "input") sortedItems = items;
+    if(sortBy === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+    if(sortBy === "packed") sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+
     return (
         <div className="list">
             <ul>
                 {
-                    items.map((item) => <Item key={item.id} item={item} onDeleteItem={onDeleteItem} onCheckedItem={onCheckedItem} />)
+                    sortedItems.map((item) => <Item key={item.id} item={item} onDeleteItem={onDeleteItem} onCheckedItem={onCheckedItem} />)
                 }
             </ul>
+            <div className="actions">
+                <select value={sortBy} onChange={(event) => setSortBy(event.target.value)}>
+                    <option value="input">Sort by input order</option>
+                    <option value="description">Sort by description</option>
+                    <option value="packed">Sort by packed status</option>
+                </select>
+            </div>
         </div>
     );
 }
